daee443f4ff656e98b579c0ccdb0e24e0c093a80
ultimate-collaborator-4688 <aalbrecht33733@gmail.com>
2020-03-12 20:16:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,5 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
-<<<<<<< HEAD
-var conversation = `Let's talk about ${topic}.`;
-=======
 var conversation = `Let's talk about ${topic}`;
->>>>>>> d81781f9454c6bbb22183574358781737eeef7a1
+
